788863b712e1eb81711150eee65216e3c8bb62da
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2020-12-17 11:41:06 -0800

    Add files via upload

--- Title Screen/title_screen.gd
+++ Title Screen/title_screen.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+onready var fallingElf = $fallingElf
+onready var animationPlayer = $fallingElf/AnimationPlayer
+onready var sleepingAnimation = $sleepingAnimation
+onready var wakeUpAnimation = $wakeUp
+
 var scene_path_to_load
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +16,13 @@ func _ready():
 		button.connect("pressed", self, "_on_Button_Pressed", [button.sceneToLoad])
 
 func _on_Button_Pressed(sceneToLoad):
+	if (sceneToLoad == "res://World.tscn"):
+		sleepingAnimation.visible = false
+		wakeUpAnimation.visible = true
+		yield(get_tree().create_timer(1), "timeout")
+		fallingElf.visible = true
+		animationPlayer.play("falling")
+		yield(get_tree().create_timer(1.2), "timeout")
 	scene_path_to_load = sceneToLoad
 	$FadeIn.show()
 	$FadeIn.fade_in()
